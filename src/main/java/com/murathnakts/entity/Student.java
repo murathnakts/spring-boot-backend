92c44de495f68a29404f9968580df4a50730b95f
murathnakts <murathanaktas111002@gmail.com>
2025-07-10 20:11:48 +0300

    Veritabanı Bağlantısı

---
+++ src/main/java/com/murathnakts/entity/Student.java
@@ -0,0 +1,30 @@
+package com.murathnakts.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "student")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "birth_of_date", nullable = true)
+    private Date birthOfDate;
+}
--- src/main/java/com/murathnakts/starter/SpringBootBackendApplication.java
+++ src/main/java/com/murathnakts/starter/SpringBootBackendApplication.java
@@ -2,8 +2,10 @@ package com.murathnakts.starter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 
+@EntityScan(basePackages = {"com.murathnakts"})
 @ComponentScan(basePackages = {"com.murathnakts"})
 @SpringBootApplication
 public class SpringBootBackendApplication {
